c19357888aa239efcffa45b1a395a0b58bc55d78
arajitdev <arajitdebnathofficial@gmail.com>
2026-03-16 23:16:32 +0530

    false nullable

--- src/main/java/com/springbootprojects/CollegeManagementSystem/Entities/StudentEntity.java
+++ src/main/java/com/springbootprojects/CollegeManagementSystem/Entities/StudentEntity.java
@@ -17,6 +17,7 @@ public class StudentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+    @Column(nullable = false)
     private String name;
     @ManyToMany(mappedBy = "studentEntities")
     @JsonIgnore
